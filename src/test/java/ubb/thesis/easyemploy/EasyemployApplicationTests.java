e70b11bcdb6a66f06dd2890de93b02e704ff80d1
carinaghiorghita <carinaioana090100@gmail.com>
2022-04-30 14:14:49 +0300

    Initial commit

---
+++ src/test/java/ubb/thesis/easyemploy/EasyemployApplicationTests.java
@@ -0,0 +1,13 @@
+package ubb.thesis.easyemploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyemployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
